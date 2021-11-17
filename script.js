f4ef59cd608b08751974d8f70e93a93796a5e4f1
dr-bells <calmacee@gmail.com>
2021-11-17 16:13:37 +0100

    Finished on close account

--- script.js
+++ script.js
@@ -205,3 +205,25 @@ btnTransfer.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    //Find index
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    //console.log(index);
+
+    //Delete account
+    accounts.splice(index, 1);
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputClosePin.value = inputCloseUsername.value = '';
+});
